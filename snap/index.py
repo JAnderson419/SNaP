b43caef5f0c958423c106934091cf8d572e4d852
Jackson Anderson <jda4923@rit.edu>
2019-04-09 12:13:56 -0400

    Control hubzero customizations with runtime arg, improved homepage view
    Signed-off-by: Jackson Anderson <jda4923@rit.edu>

--- snap/index.py
+++ snap/index.py
@@ -4,14 +4,25 @@ Created on Tue Mar 26 14:57:14 2019
 
 @author: ander906
 """
-from os.path import join, dirname, abspath
+import sys
+from os.path import join, dirname, abspath, basename
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from app import app
-from apps import app_viewer
+hubzero=False
 
+for arg in sys.argv:
+    if arg == 'hubzero':
+        hubzero=True
+    else:
+        continue
+
+if hubzero:
+    from hubzeroapp import app
+else:
+    from app import app
+from apps import app_viewer
 
 changes = ''
 with open(join(dirname(dirname(abspath(__file__))), 'CHANGELOG.md')) as f:
@@ -26,23 +37,40 @@ app.layout = html.Div([
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/':
+    try:  # Need to take basename to handle nanohub proxy
+        if basename(pathname):
+            pathname=basename(pathname)
+    except TypeError:  # On start of app, pathname will be NoneType, throwing error unless caught
+        pass
+    if pathname == 'app_viewer':
+        return app_viewer.layout
+    else:
         return html.Div([
+            html.A(
+                children=html.Img(src=app.get_asset_url('github2.svg')),
+                href=r'https://github.com/JAnderson419/SNaP',
+                style={'fill': r'#151513', 'color': r'#fff', 'position': 'absolute', 'top': 0, 'border': 0, 'right': 0}
+            ),
             html.H1('SNaP SnP Utilities'),
             html.H3(children='by Jackson Anderson, ander906@purdue.edu'),
             html.Hr(),
             html.H5('Release Notes'),
-            dcc.Markdown(children=changes),
+            html.Div(
+                dcc.Markdown(children=changes),
+                style={'border': '2px solid #a3a3c2', 'background-color': r'#f0f0f5',
+                       'height': r'10em', 'overflow': 'scroll', 'resize': 'both'}
+            ),
             html.Hr(),
-            dcc.Link('Go to SnP Viewer.', href='/apps/app_viewer'),
-            html.Footer('Powered by scikit-rf and Dash.')
+            dcc.Link('Go to SnP Viewer.', href='app_viewer'),
+            html.A(
+                children=html.Img(src=app.get_asset_url('powered_by_scikit-rf.svg')),
+                href=r'http://scikit-rf.org',
+                style={'position': 'absolute', 'bottom': 0, 'right': 0}
+            ),
         ])
-    elif pathname == '/apps/app_viewer':
-        return app_viewer.layout
-    else:
-        return '404'
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
-    # hubzero requres these port and host settings
-    # app.run_server(port=8000, host='0.0.0.0')
+    if hubzero:
+        app.run_server(port=8000, host='0.0.0.0')
+    else:
+        app.run_server(debug=True)
